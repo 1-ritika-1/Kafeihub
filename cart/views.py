d63c73a6eb35578b4f7a72704b4685f32bfdedf8
Farhaanbg <muhammad.beegoo@umail.uom.ac.mu>
2025-11-30 13:35:16 +0400

    Added popup and minor fixes to item page

--- cart/views.py
+++ cart/views.py
@@ -2,26 +2,36 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from menu.models import MenuItem
 from .models import CartItem
+from django.http import JsonResponse
 
-# ADD TO CART
+
+# ADD TO CART (AJAX ENABLED)
 @login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(MenuItem, id=product_id)
 
-    # print("ADD TO CART CALLED!", product_id) -for debug
+    if request.method == "POST":
+        quantity = int(request.POST.get("quantity", 1))
 
-    cart_item, created = CartItem.objects.get_or_create(
-        user=request.user,
-        product=product,
-        defaults={'quantity': 1}
-    )
+        cart_item, created = CartItem.objects.get_or_create(
+            user=request.user,
+            product=product,
+            defaults={'quantity': quantity}
+        )
 
-    # If item already in cart → increase quantity
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
+        # If already exists, increase by chosen quantity
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
 
-    return redirect("cart:view_cart")
+        # If AJAX request -> return JSON instead of redirect
+        if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+            return JsonResponse({"success": True, "message": "Item added to cart!"})
+
+        return redirect("cart:view_cart")
+
+    return JsonResponse({"success": False}, status=400)
+
 
 
 # VIEW CART
